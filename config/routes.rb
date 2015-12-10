aef4df71825e0a7ffcffd6780fae4234da3c490e
Jo Kakomba <ariel.kakomba@gmail.com>
2015-12-10 07:35:16 +0000

    added login and logout and authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,11 @@ Rails.application.routes.draw do
   
   get '/register', to: 'chefs#new'
   
+  get '/login', to: 'logins#new'
+  
+  post '/login', to: 'logins#create'
+  
+  get '/logout', to: 'logins#destroy'
+  
   
 end
